893a2e1de2bc495025a461a14ab6dfd26a4b759c
EnzoMelgarejo <enzomelgarejo@gmail.com>
2024-10-21 11:25:43 -0300

    Agregado de navbar con menu desplegable (falta cambiar el mapeo del menu por flatlist)

--- B2B-Mobile/app/_layout.tsx
+++ B2B-Mobile/app/_layout.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { View } from "react-native";
 import { Slot } from 'expo-router';
 import Navbar from "@/components/navbar";
-
+import { StatusBar } from "expo-status-bar";
 export default function Layout() {
     return (
         <View>
+            <StatusBar backgroundColor="#000000" />
             <Navbar />
             <Slot />
         </View>
