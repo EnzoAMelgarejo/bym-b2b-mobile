c7e5245cad3899a9f06bf12cf2aef69b5163fef6
EnzoMelgarejo <enzomelgarejo@gmail.com>
2024-10-29 20:44:13 -0300

    Finalizacion de seccion blog con menu

--- B2B-Mobile/components/blogComponets/blogFilter.tsx
+++ B2B-Mobile/components/blogComponets/blogFilter.tsx
@@ -1,151 +1,139 @@
-// components/FilterMenu.js
+// components/BlogMenu/BlogMenu.tsx
 import React from "react";
-import { View, Text, Modal, StyleSheet, Pressable, FlatList, Image } from "react-native";
+import { View, Modal, ScrollView, FlatList, StyleSheet, Pressable, Image, Text } from "react-native";
+import PopularPost from "./popularPost";
+import SubscribeSection from "./subscribe";
+import InstagramSection from "./instagramSection";
 import { AntDesign } from "@expo/vector-icons";
 
-const BlogMenu = ({ modalVisible, toggleModal }) => {
-  const categories = ["Elemento 1", "Elemento 2", "Elemento 3"];
-  const post = {
-    image: require("../../assets/images/Home02.png"),
-    date: "2024-10-25",
-    title: "Título del Post",
-  };
-  const popularPosts = [
-    { id: 1, image: require("../../assets/images/Home02.png"), title: "Popular Post 1" },
-    { id: 2, image: require("../../assets/images/Home02.png"), title: "Popular Post 2" },
-    { id: 3, image: require("../../assets/images/Home02.png"), title: "Popular Post 3" },
-  ];
+interface BlogMenuProps {
+  modalVisible: boolean;
+  toggleModal: () => void;
+}
 
-  return (
-    <Modal
-      transparent={true}
-      visible={modalVisible}
-      animationType="slide"
-      onRequestClose={toggleModal}
-    >
+const categories = ["Elemento 1", "Elemento 2", "Elemento 3"];
+const post = {
+  image: require("../../assets/images/Home02.png"),
+  date: "2024-10-25",
+  title: "Título del Post",
+};
+const popularPosts = [
+  { id: 1, image: require("../../assets/images/Home02.png"), title: "Popular Post 1" },
+  { id: 2, image: require("../../assets/images/Home02.png"), title: "Popular Post 2" },
+  { id: 3, image: require("../../assets/images/Home02.png"), title: "Popular Post 3" },
+];
+const instagramImages = [
+  require("../../assets/images/Home02.png"),
+  require("../../assets/images/Home02.png"),
+  require("../../assets/images/Home02.png"),
+  require("../../assets/images/Home02.png"),
+];
+
+const BlogMenu: React.FC<BlogMenuProps> = ({ modalVisible, toggleModal }) => (
+  <Modal transparent={true} visible={modalVisible} animationType="slide" onRequestClose={toggleModal}>
+    <ScrollView>
       <View style={styles.modalOverlay}>
         <View style={styles.modalContent}>
-
           <Text style={styles.modalTitle}>Browse Categories:</Text>
           <FlatList
             data={categories}
-            renderItem={({ item }) => (
-              <Text style={styles.categoryItem}>{item}</Text>
-            )}
+            renderItem={({ item }) => <Text style={styles.categoryItem}>{item}</Text>}
             keyExtractor={(item) => item}
           />
           <View style={styles.separator} />
 
-          <View style={styles.postRow}>
-          <Text style={styles.modalTitle}>Post:</Text>
-            <View style={styles.navigationIcons}>
-              <AntDesign name="left" size={20} color="#333" />
-              <AntDesign name="right" size={20} color="#333" />
-            </View>
+          <View style={styles.titleContainer}>
+            <Text style={styles.modalTitle}>Post:</Text>
+            <AntDesign name="left" size={20} color="#555" style={styles.icon} />
+            <AntDesign name="right" size={20} color="#555" style={styles.icon} />
           </View>
           <Image source={post.image} style={styles.postImage} />
-            <Text style={styles.postLabel}>Título del Post</Text>
           <Text style={styles.postDetails}>Fecha de Publicación: {post.date}</Text>
-          <Text style={styles.postDetails}>{post.title}</Text>
+          <Text style={styles.postLabel}>{post.title}</Text>
           <View style={styles.separator} />
 
           <Text style={styles.modalTitle}>Popular Post:</Text>
           {popularPosts.map((popular) => (
-            <View key={popular.id} style={styles.popularPostRow}>
-              <Image source={popular.image} style={styles.popularImage} />
-              <Text style={styles.popularTitle}>{popular.title}</Text>
-            </View>
+            <PopularPost key={popular.id} image={popular.image} title={popular.title} />
           ))}
+
+          <SubscribeSection />
+
+          <View style={styles.separator} />
+
+          <InstagramSection instagramImages={instagramImages} />
 
           <Pressable onPress={toggleModal} style={styles.closeButton}>
             <Text style={styles.closeButtonText}>Cerrar</Text>
           </Pressable>
-
         </View>
       </View>
-    </Modal>
-  );
-};
+    </ScrollView>
+  </Modal>
+);
 
+// Estilos
 const styles = StyleSheet.create({
   modalOverlay: {
     flex: 1,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    backgroundColor: "rgba(0,0,0,0.5)",
     justifyContent: "center",
     alignItems: "center",
   },
   modalContent: {
-    width: '90%',
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
     padding: 20,
+    width: "90%",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  icon: {
+    left: 200,
   },
   modalTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  separator: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    marginVertical: 15,
+  },
+  postImage: {
+    width: "100%",
+    height: 200,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  postDetails: {
+    fontSize: 14,
+    color: "#555",
+  },
+  postLabel: {
     fontSize: 18,
     fontWeight: "bold",
-    marginTop: 10,
-    marginBottom: 5,
+    color: "#333",
+  },
+  closeButton: {
+    backgroundColor: "#000",
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  closeButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
   },
   categoryItem: {
     fontSize: 16,
     color: "#333",
     paddingVertical: 5,
-  },
-  separator: {
-    height: 1,
-    backgroundColor: "#ddd",
-    marginVertical: 10,
-  },
-  postRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginVertical: 10,
-  },
-  postLabel: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  navigationIcons: {
-    flexDirection: "row",
-    gap: 10,
-  },
-  postImage: {
-    width: "100%",
-    height: 150,
-    marginVertical: 10,
-    borderRadius: 8,
-  },
-  postDetails: {
-    fontSize: 14,
-    color: "#555",
-    marginBottom: 5,
-  },
-  popularPostRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginVertical: 5,
-  },
-  popularImage: {
-    width: 40,
-    height: 40,
-    marginRight: 10,
-    borderRadius: 5,
-  },
-  popularTitle: {
-    fontSize: 16,
-    color: "#333",
-  },
-  closeButton: {
-    backgroundColor: "#FF9C2A",
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 20,
-    alignSelf: "center",
-  },
-  closeButtonText: {
-    color: "#fff",
-    fontWeight: "bold",
   },
 });
 
